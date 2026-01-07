993fe135b6419c0a11a71d6313ef6c5bac5819e6
Feride Ismayilova <ferideismayilova340@gmail.com>
2026-01-07 20:36:52 +0400

    oo add

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        // hello
+        // helloooo
 
         Scanner sc = new Scanner(System.in);
         boolean shert = true;
