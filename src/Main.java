23864ae51de74f56e9e5ee3860d1464c58d7f909
imranrasulzade <imranrasulzade5@gmail.com>
2026-01-07 20:40:28 +0400

    hdshk add

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        // helloooo
+        // hellooootrgtr
 
         Scanner sc = new Scanner(System.in);
         boolean shert = true;
