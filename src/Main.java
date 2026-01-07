907d3375d854a83645454b4310cbcf0ca9412d14
Imran Rasulzade <93717399+imranrasulzade@users.noreply.github.com>
2026-01-07 20:20:09 +0400

    Add comment in main method of Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        // hello
 
         Scanner sc = new Scanner(System.in);
         boolean shert = true;
